7c6d4d4e3c9bf3af3097e183120d9b4397daaa32
paris <pariskasidiaris@gmail.com>
2014-03-24 17:24:12 +0000

    Implemented default terminal colors in CSS

--- src/xterm.js
+++ src/xterm.js
@@ -305,27 +305,6 @@ Terminal.tangoColors = [
   '#eeeeec'
 ];
 
-Terminal.xtermColors = [
-  // dark:
-  '#000000', // black
-  '#cd0000', // red3
-  '#00cd00', // green3
-  '#cdcd00', // yellow3
-  '#0000ee', // blue2
-  '#cd00cd', // magenta3
-  '#00cdcd', // cyan3
-  '#e5e5e5', // gray90
-  // bright:
-  '#7f7f7f', // gray50
-  '#ff0000', // red
-  '#00ff00', // green
-  '#ffff00', // yellow
-  '#5c5cff', // rgb:5c/5c/ff
-  '#ff00ff', // magenta
-  '#00ffff', // cyan
-  '#ffffff'  // white
-];
-
 // Colors 0-15 + 16-255
 // Much thanks to TooTallNate for writing this.
 Terminal.colors = (function() {
@@ -357,10 +336,6 @@ Terminal.colors = (function() {
 
   return colors;
 })();
-
-// Default BG/FG
-Terminal.colors[256] = '#000000';
-Terminal.colors[257] = '#f0f0f0';
 
 Terminal._colors = Terminal.colors.slice();
 
