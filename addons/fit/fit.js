a249d2714f8eebacd7d099f95192a97aa3178acd
paris <pariskasidiaris@gmail.com>
2014-08-19 13:24:43 +0000

    Fix terminal fitting height

--- addons/fit/fit.js
+++ addons/fit/fit.js
@@ -14,22 +14,32 @@
 Terminal.prototype.fit = function () {
   var container = this.rowContainer,
       subjectRow = this.rowContainer.firstElementChild,
-      rows = parseInt(container.offsetHeight / subjectRow.offsetHeight),
+      rows,
       contentBuffer,
       characterWidth,
+      characterHeight,
       cols;
-  
+
   subjectRow.style.display = 'inline';
   
   contentBuffer = subjectRow.textContent;
 
-  subjectRow.textContent = ' ';
+  subjectRow.innerHTML = '&nbsp;'; /* Arbitrary character to calculate its dimensions */
   characterWidth = parseInt(subjectRow.offsetWidth);
+  characterHeight = parseInt(subjectRow.offsetHeight);
 
   subjectRow.style.display = '';
   
   cols = container.offsetWidth / characterWidth;
   cols = parseInt(cols);
+  
+  var parentElementStyle = window.getComputedStyle(this.element.parentElement),
+      parentElementHeight = parseInt(parentElementStyle.getPropertyValue('height')),
+      elementStyle = window.getComputedStyle(this.element),
+      elementPadding = parseInt(elementStyle.getPropertyValue('padding-top')) + parseInt(elementStyle.getPropertyValue('padding-bottom')),
+      availableHeight = parentElementHeight - elementPadding;
+  
+    rows = parseInt(availableHeight / characterHeight);
       
   this.resize(cols, rows);
 };
