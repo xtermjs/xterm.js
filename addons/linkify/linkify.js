b60903a2aec137738c394d1b79e82763b44bc959
paris <paris@sourcelair.com>
2015-01-04 15:08:36 +0200

    [addon linkify] Start working on "linkifying" URLs in the terminal

---
+++ addons/linkify/linkify.js
@@ -0,0 +1,27 @@
+(function (linkify) {
+})(function (Xterm) {
+    Xterm.prototype.linkify = function () {
+      var rows = this.rowContainer.children,
+          buffer = document.createElement('span');
+
+      for (var i=0; i<rows.length; i++) {
+        var line = rows[i], nodes = line.childNodes;
+
+        for (var j=0; j<nodes.length; j++) {
+          var node = nodes[j];
+
+          if (node.nodeType == 3) {
+              var match = node.data.match(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/);
+
+            if (match) {
+              var url=match[0],
+                  newData = node.data.replace(url, '<a href="http://' +  url + '" target="_blank" >' + url + '</a>');
+              buffer.textContent = node.data;
+              line.innerHTML = line.innerHTML.replace(buffer.innerHTML, newData);
+              this.emit('linkify:line', line);
+            }
+          }
+        }
+      }
+    };
+});
