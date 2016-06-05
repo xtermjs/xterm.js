53e8df40bb36783137452f53099f1dbe3ef58194
Paris <paris@sourcelair.com>
2016-06-05 08:22:22 +0300

    Implement fully featured terminal demo w/ static fallback

---
+++ demo/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+var pty = require('pty.js');
+
+app.use('/src', express.static(__dirname + '/../src'));
+app.use('/addons', express.static(__dirname + '/../addons'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function(req, res){
+  res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/main.js', function(req, res){
+  res.sendFile(__dirname + '/main.js');
+});
+
+app.ws('/bash', function(ws, req) {
+  /**
+   * Open bash terminal and attach it
+   */
+  var term = pty.spawn('bash', [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 24,
+    cwd: process.env.PWD,
+    env: process.env
+  });
+
+  term.on('data', function(data) {
+    ws.send(data);
+  });
+  ws.on('message', function(msg) {
+    term.write(msg);
+  });
+  ws.on('close', function () {
+    console.log('close');
+    process.kill(term.pid);
+  });
+});
+
+var port = process.env.PORT || 3000,
+    host = '0.0.0.0';
+
+console.log('App listening to ' + host + ':' + port);
+app.listen(port, host);
--- demo/main.js
+++ demo/main.js
@@ -1,38 +1,60 @@
 var terminalContainer = document.getElementById('terminal-container'),
     term = new Terminal(),
-    shellprompt = '$ ';
+    protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://',
+    socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + '/bash',
+    socket = new WebSocket(socketURL);
 
 term.open(terminalContainer);
 term.fit();
 
-term.prompt = function () {
-  term.write('\r\n' + shellprompt);
-};
+function runRealTerminal() {
+  term.attach(socket);
+  term._initialized = true;
+}
 
-term.writeln('Welcome to xterm.js');
-term.writeln('Just type some keys in the prompt below.');
-term.writeln('');
-term.prompt();
+function runFakeTerminal() {
+  if (term._initialized) {
+    return;
+  }
 
-term.on('key', function (key, ev) {
-  var printable = (
-    !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
-  );
+  term._initialized = true;
 
-  if (ev.keyCode == 13) {
-    term.prompt();
-  } else if (ev.keyCode == 8) {
-    /*
+  var shellprompt = '$ ';
+
+  term.prompt = function () {
+    term.write('\r\n' + shellprompt);
+  };
+
+  term.writeln('Welcome to xterm.js');
+  term.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
+  term.writeln('Type some keys and commands to play around.');
+  term.writeln('');
+  term.prompt();
+
+  term.on('key', function (key, ev) {
+    var printable = (
+      !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+    );
+
+    if (ev.keyCode == 13) {
+      term.prompt();
+    } else if (ev.keyCode == 8) {
+      /*
      * Do not delete the prompt
      */
-    if (term.x > 2) {
-      term.write('\b \b');
+      if (term.x > 2) {
+        term.write('\b \b');
+      }
+    } else if (printable) {
+      term.write(key);
     }
-  } else if (printable) {
-    term.write(key);
-  }
-});
+  });
 
-term.on('paste', function (data, ev) {
-  term.write(data);
-});
+  term.on('paste', function (data, ev) {
+    term.write(data);
+  });
+}
+
+socket.onopen = runRealTerminal;
+socket.onclose = runFakeTerminal;
+socket.onerror = runFakeTerminal;
