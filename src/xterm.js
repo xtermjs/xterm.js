da9f86f1a7764b50a03ee8fcd77d12a660c8b5b0
Daniel Imms <daimms@microsoft.com>
2016-06-09 19:59:59 -0700

    Draw cursor at correct position when scrolling
    Fixes #64

--- src/xterm.js
+++ src/xterm.js
@@ -1102,9 +1102,8 @@
         line = this.lines[row];
         out = '';
 
-        if (y === this.y
+        if (this.y === y - (this.ybase - this.ydisp)
             && this.cursorState
-            && (this.ydisp === this.ybase)
             && !this.cursorHidden) {
           x = this.x;
         } else {
