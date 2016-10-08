8117999ab3681e940aec26cbfc2809287e749e83
Paris Kasidiaris <paris@sourcelair.com>
2016-10-08 12:22:18 +0300

    Fix tests

--- test/addons/linkify-test.js
+++ test/addons/linkify-test.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-var Terminal = require('../../src/xterm');
+var Terminal = require('../../dist/xterm');
 var linkify = require('../../addons/linkify/linkify');
 
 describe('linkify addon', function () {
