69a3f9a2b40977521adf5cc73e5499dfa2fbf65f
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-30 12:14:06 +0100

    feat(#325): add line cache to support resizing

--- src/xterm.js
+++ src/xterm.js
@@ -235,6 +235,11 @@ function Terminal(options) {
     this.lines.push(this.blankLine());
   }
 
+  /**
+   * An array of lines, corresponding to this.lines to cache characters in case of a resize
+   */
+  this.lineCache = this.lines.map(l => this.blankLine())
+
   this.tabs;
   this.setupStops();
 
@@ -2861,6 +2866,7 @@ Terminal.prototype.error = function() {
  * @param {number} x The number of columns to resize to.
  * @param {number} y The number of rows to resize to.
  */
+
 Terminal.prototype.resize = function(x, y) {
   var line
   , el
@@ -2883,14 +2889,18 @@ Terminal.prototype.resize = function(x, y) {
     i = this.lines.length;
     while (i--) {
       while (this.lines[i].length < x) {
-        this.lines[i].push(ch);
+        this.lineCache[i] = this.lineCache[i] || []
+        const toAdd = this.lineCache[i].pop() || ch
+        this.lines[i].push(toAdd);
       }
     }
   } else { // (j > x)
     i = this.lines.length;
     while (i--) {
       while (this.lines[i].length > x) {
-        this.lines[i].pop();
+        const popped = this.lines[i].pop()
+        this.lineCache[i] = this.lineCache[i] || []
+        this.lineCache[i].push(popped)
       }
     }
   }
