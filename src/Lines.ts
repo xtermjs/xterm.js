b1e9196e14a24464d6c6ea9c5ccd5f999612f8c5
Paris Kasidiaris <paris@sourcelair.com>
2016-12-14 13:33:35 +0200

    Implement baseline for `Lines` class

---
+++ src/Lines.ts
@@ -0,0 +1,38 @@
+/**
+ * @license MIT
+ */
+
+type Char = [number, string, number];
+type Line = Char[];  // Array of characters in tuple format
+
+export class Lines {
+  private data: Line[];  // Array of lines
+
+  constructor(data: Line[] = []) {
+    this.data = data;
+  }
+
+  get length() {
+    return this.data.length;
+  }
+
+  get(index: number) {
+    return this.data[index];
+  }
+
+  set(index: number, value: Line) {
+    return this.data[index] = value;
+  }
+
+  push(line: Line) {
+    return this.data.push(line);
+  }
+
+  slice(begin: number, end?: number) {
+    return this.data.slice(begin, end);
+  }
+
+  splice(start: number, deleteCount: number, ...items: Line[]) {
+    return this.data.splice.apply(this.data, [start, deleteCount, ...items]);
+  }
+}
