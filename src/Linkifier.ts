a10ee8f58c5d97f7e520872a2c5ebc5c03abc295
Daniel Imms <daimms@microsoft.com>
2017-02-17 10:18:37 -0800

    Revert priority change, http handler needs to be top

--- src/Linkifier.ts
+++ src/Linkifier.ts
@@ -123,7 +123,7 @@ export class Linkifier {
       return;
     }
     const text = row.textContent;
-    for (let i = this._linkMatchers.length - 1; i >= 0; i--) {
+    for (let i = 0; i < this._linkMatchers.length; i--) {
       const matcher = this._linkMatchers[i];
       const uri = this._findLinkMatch(text, matcher.regex, matcher.matchIndex);
       if (uri) {
