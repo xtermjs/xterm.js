5b3f724deb2f4bd65495cfcfe70d4d2d65354244
Aleksandr Andrienko <aandrienko@codenvy.com>
2017-06-25 21:42:31 +0300

    Fix turning from alt screen to normal screen and vice versa. Fix https://github.com/sourcelair/xterm.js/issues/229 .

--- src/xterm.js
+++ src/xterm.js
@@ -154,7 +154,6 @@ function Terminal(options) {
   this.originMode = false;
   this.insertMode = false;
   this.wraparoundMode = true; // defaults: xterm - true, vt100 - false
-  this.normal = null;
 
   // charset
   this.charset = null;
@@ -221,10 +220,12 @@ function Terminal(options) {
   this.surrogate_high = '';
 
   // Create the terminal's buffers and set the current buffer
-  this.buffers = new BufferSet(this);
-  this.buffer = this.buffers.active;  // Convenience shortcut;
+  if (!this.buffers) {
+    this.buffers = new BufferSet(this);
+    this.buffer = this.buffers.active;  // Convenience shortcut;
+  }
   this.buffers.on('activate', function (buffer) {
-    this.buffer = buffer;
+    this._terminal.buffer = buffer;
   });
 
   /**
@@ -1947,8 +1948,6 @@ Terminal.prototype.resize = function(x, y) {
 
   this.refresh(0, this.rows - 1);
 
-  this.normal = null;
-
   this.geometry = [this.cols, this.rows];
   this.emit('resize', {terminal: this, cols: x, rows: y});
 };
@@ -2221,9 +2220,13 @@ Terminal.prototype.reset = function() {
   this.options.cols = this.cols;
   var customKeydownHandler = this.customKeydownHandler;
   var cursorBlinkInterval = this.cursorBlinkInterval;
+  var inputHandler = this.inputHandler;
+  var buf = this.buffers;
   Terminal.call(this, this.options);
   this.customKeydownHandler = customKeydownHandler;
   this.cursorBlinkInterval = cursorBlinkInterval;
+  this.inputHandler = inputHandler;
+  this.buffers = buf;
   this.refresh(0, this.rows - 1);
   this.viewport.syncScrollArea();
 };
