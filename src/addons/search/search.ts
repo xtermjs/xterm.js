fab039ddf3ce5f07ea9df7fd8a61e3f896cf9c42
Daniel Imms <daimms@microsoft.com>
2017-07-09 00:26:33 -0700

    Rearrange module loader to work with browserify

--- src/addons/search/search.ts
+++ src/addons/search/search.ts
@@ -8,24 +8,25 @@ declare var exports: any;
 declare var module: any;
 declare var define: any;
 declare var require: any;
+declare var window: any;
 
 (function (addon) {
   if ('Terminal' in window) {
-    /*
+    /**
      * Plain browser environment
      */
-    addon((<any>window).Terminal);
+    addon(window.Terminal);
+  } else if (typeof exports === 'object' && typeof module === 'object') {
+    /**
+     * CommonJS environment
+     */
+    const xterm = '../../xterm';
+    module.exports = addon(require(xterm));
   } else if (typeof define == 'function') {
-    /*
+    /**
      * Require.js is available
      */
     define(['../../xterm'], addon);
-  } else if (typeof exports === 'object' && typeof module === 'object') {
-    /*
-     * CommonJS environment
-     */
-    var xterm = '../../xterm'; // Put in a variable do it's not pulled in by browserify
-    module.exports = addon(require(xterm));
   }
 })((Terminal: any) => {
   /**
