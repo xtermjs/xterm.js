2878d978c4e38473ab3843e6c3085a7e24e47fae
Daniel Imms <daimms@microsoft.com>
2017-08-05 21:06:04 -0700

    Fix lint

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -46,7 +46,7 @@ export interface ITerminal extends IEventEmitter {
   log(text: string): void;
   reset(): void;
   showCursor(): void;
-  blankLine(cur?: boolean, isWrapped?: boolean);
+  blankLine(cur?: boolean, isWrapped?: boolean): [number, string, number];
 }
 
 /**
