0a58628f74b57953d7fec4feef55031f7acb5a5a
Daniel Imms <daimms@microsoft.com>
2017-09-13 23:49:34 -0700

    Clear text layer to the background color on creation
    Prevents a black flash before the first draw

--- src/renderer/BaseRenderLayer.ts
+++ src/renderer/BaseRenderLayer.ts
@@ -33,6 +33,10 @@ export abstract class BaseRenderLayer implements IRenderLayer {
     this._canvas.style.zIndex = zIndex.toString();
     this._ctx = this._canvas.getContext('2d', {alpha});
     this._ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    // Draw the background if this is an opaque layer
+    if (!alpha) {
+      this.clearAll();
+    }
     container.appendChild(this._canvas);
   }
 
