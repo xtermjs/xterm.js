9fe8fffa4a2defda833ae7545b19674f7ed6ac93
Guillermo Zunino <guillez@gmail.com>
2017-10-16 17:31:25 -0300

    one terminal for 2 backens

--- demo/app.js
+++ demo/app.js
@@ -24,7 +24,9 @@ app.get('/main.js', function(req, res){
 app.post('/terminals', function (req, res) {
   var cols = parseInt(req.query.cols),
       rows = parseInt(req.query.rows),
-      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+      cmd = req.query.cmd,
+      args = req.query.args ? ['-c', cmd + ' ' + req.query.args] : [],
+      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', args || [], {
         name: 'xterm-color',
         cols: cols || 80,
         rows: rows || 24,
@@ -64,6 +66,10 @@ app.ws('/terminals/:pid', function (ws, req) {
     } catch (ex) {
       // The WebSocket is not open, ignore
     }
+  });
+  term.on('exit', function(exit) {
+    console.log('exited', term.pid, 'code', exit);
+    ws.close(3001);
   });
   ws.on('message', function(msg) {
     term.write(msg);
