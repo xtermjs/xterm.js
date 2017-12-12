80450450e91abff3deb9426ce3d408ef4ad2331b
Paris Kasidiaris <paris@sourcelair.com>
2017-12-12 10:50:18 +0200

    Export zmodemAttach

--- src/addons/zmodem/zmodem.ts
+++ src/addons/zmodem/zmodem.ts
@@ -27,63 +27,62 @@
  *  via `detach()` and a re-`attach()`.)
  */
 
+export function zmodemAttach(term, ws, opts) {
+  if (!opts) opts = {};
+
+  var senderFunc = function _ws_sender_func(octets) {
+    ws.send(new Uint8Array(octets));
+  };
+
+  var zsentry;
+
+  function _shouldWrite() {
+    return !!zsentry.get_confirmed_session() || !opts.noTerminalWriteOutsideSession;
+  }
+
+  zsentry = new Zmodem.Sentry( {
+    to_terminal: function _to_terminal(octets) {
+      if (_shouldWrite()) {
+        term.write(
+          String.fromCharCode.apply(String, octets)
+        );
+      }
+    },
+
+    sender: senderFunc,
+
+    on_retract: function _on_retract() {
+      term.emit('zmodemRetract');
+    },
+
+    on_detect: function _on_detect(detection) {
+      term.emit('zmodemDetect', detection);
+    },
+  } );
+
+  function handleWSMessage(evt) {
+
+    //In testing with xterm.js’s demo the first message was
+    //always text even if the rest were binary. While that
+    //may be specific to xterm.js’s demo, ultimately we
+    //should reject anything that isn’t binary.
+    if (typeof evt.data === 'string') {
+      if (_shouldWrite()) {
+        term.write(evt.data);
+      }
+    }
+    else {
+      zsentry.consume(evt.data);
+    }
+  }
+
+  ws.binaryType = 'arraybuffer';
+  ws.addEventListener('message', handleWSMessage);
+}
+
 export function apply(terminalConstructor) {
   let Zmodem = (typeof window == 'object') ? (<any>window).ZModem : {Browser: null};  // Nullify browser for tests
 
-  terminalConstructor.prototype.zmodemAttach = function(ws, opts) {
-    var term = this;
-
-    if (!opts) opts = {};
-
-    var senderFunc = function _ws_sender_func(octets) {
-      ws.send( new Uint8Array(octets) );
-    };
-
-    var zsentry;
-
-    function _shouldWrite() {
-      return !!zsentry.get_confirmed_session() || !opts.noTerminalWriteOutsideSession;
-    }
-
-    zsentry = new Zmodem.Sentry( {
-      to_terminal: function _to_terminal(octets) {
-        if (_shouldWrite()) {
-          term.write(
-            String.fromCharCode.apply(String, octets)
-          );
-        }
-      },
-
-      sender: senderFunc,
-
-      on_retract: function _on_retract() {
-        term.emit('zmodemRetract');
-      },
-
-      on_detect: function _on_detect(detection) {
-        term.emit('zmodemDetect', detection);
-      },
-    } );
-
-    function handleWSMessage(evt) {
-
-      //In testing with xterm.js’s demo the first message was
-      //always text even if the rest were binary. While that
-      //may be specific to xterm.js’s demo, ultimately we
-      //should reject anything that isn’t binary.
-      if (typeof evt.data === 'string') {
-        if (_shouldWrite()) {
-          term.write(evt.data);
-        }
-      }
-      else {
-        zsentry.consume(evt.data);
-      }
-    }
-
-    ws.binaryType = 'arraybuffer';
-    ws.addEventListener('message', handleWSMessage);
-  }
-
+  terminalConstructor.prototype.zmodemAttach = zmodemAttach.bind(this, this);
   terminalConstructor.prototype.zmodemBrowser = Zmodem.Browser
 }
