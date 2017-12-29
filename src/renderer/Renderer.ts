72d946e089a4014e510f5d60751c391444e5f891
Daniel Imms <daimms@microsoft.com>
2017-12-29 13:54:01 -0800

    Use a more robust solution to changing devicePixelRatio

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -13,6 +13,7 @@ import { BaseRenderLayer } from './BaseRenderLayer';
 import { IRenderLayer, IColorSet, IRenderer, IRenderDimensions } from './Interfaces';
 import { LinkRenderLayer } from './LinkRenderLayer';
 import { EventEmitter } from '../EventEmitter';
+import { ScreenDprMonitor } from '../utils/ScreenDprMonitor';
 
 export class Renderer extends EventEmitter implements IRenderer {
   /** A queue of the rows to be refreshed */
@@ -21,6 +22,7 @@ export class Renderer extends EventEmitter implements IRenderer {
 
   private _renderLayers: IRenderLayer[];
   private _devicePixelRatio: number;
+  private _screenDprMonitor: ScreenDprMonitor;
 
   public colorManager: ColorManager;
   public dimensions: IRenderDimensions;
@@ -53,6 +55,9 @@ export class Renderer extends EventEmitter implements IRenderer {
     };
     this._devicePixelRatio = window.devicePixelRatio;
     this._updateDimensions();
+
+    this._screenDprMonitor = new ScreenDprMonitor();
+    this._screenDprMonitor.setListener(() => this.onWindowResize(window.devicePixelRatio));
   }
 
   public onWindowResize(devicePixelRatio: number): void {
