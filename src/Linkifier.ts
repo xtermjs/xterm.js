a9d3e61794c384efeed16eda220727da707314d0
Daniel Imms <daimms@microsoft.com>
2018-01-22 12:43:42 -0800

    Fix tests

--- src/Linkifier.ts
+++ src/Linkifier.ts
@@ -97,7 +97,7 @@ export class Linkifier extends EventEmitter implements ILinkifier {
     if (this._rowsTimeoutId) {
       clearTimeout(this._rowsTimeoutId);
     }
-    this._rowsTimeoutId = window.setTimeout(() => this._linkifyRows(), Linkifier.TIME_BEFORE_LINKIFY);
+    this._rowsTimeoutId = <number><any>setTimeout(() => this._linkifyRows(), Linkifier.TIME_BEFORE_LINKIFY);
   }
 
   /**
