99aa142cefe8f1da1c04cc07425d26ec50ed3c08
Bruno Ribeito <b.m.fernandes.ribeiro@gmail.com>
2018-02-19 23:55:25 +0000

    Fix test -> wrong Regex flag

--- src/renderer/ColorManager.ts
+++ src/renderer/ColorManager.ts
@@ -66,7 +66,7 @@ function toPaddedHex(c: number): string {
  * Manages the source of truth for a terminal's colors.
  */
 export class ColorManager implements IColorManager {
-  private VALID_NON_NAMED_COLORS = new RegExp('^(#[0-9a-f]{3}|#(?:[0-9a-f]{2}){2,4}|(rgb|hsl)a?\((-?\d+%?[,\s]+){2,3}\s*[\d\.]+%?\))$', 'g');
+  private VALID_NON_NAMED_COLORS = new RegExp('^(#[0-9a-f]{3}|#(?:[0-9a-f]{2}){2,4}|(rgb|hsl)a?\((-?\d+%?[,\s]+){2,3}\s*[\d\.]+%?\))$', 'i');
 
   public colors: IColorSet;
 
