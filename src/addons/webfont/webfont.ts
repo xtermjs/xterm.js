b6ba7439ea3634f3c006cf8f284141d9a09aee94
Vincent Woo <me@vincentwoo.com>
2018-04-03 18:44:35 -0700

    Addon that refreshes xterm upon webfont load

---
+++ src/addons/webfont/webfont.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../../typings/xterm.d.ts"/>
+
+import { Terminal } from 'xterm';
+
+export function apply(terminalConstructor: typeof Terminal): void {
+  (<any>terminalConstructor.prototype).loadWebfontAndOpen = function (element: HTMLElement): void {
+    const FontFaceObserver = (typeof window === 'object' && (<any>window).FontFaceObserver);
+    if (!FontFaceObserver) {
+      console.warn('FontFaceObserver not available, opening xterm normally!');
+      return this.open(element);
+    }
+    const regular = new FontFaceObserver(this.options.fontFamily).load();
+    const bold = new FontFaceObserver(this.options.fontFamily, { weight: 'bold' }).load();
+
+    regular.constructor.all([regular, bold]).then(() => {
+      this.open(element);
+    });
+  };
+}
