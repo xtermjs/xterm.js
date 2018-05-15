2b4c5adb3c86a0049d6fbbd5f233eb3c83b7108a
Daniel Imms <daimms@microsoft.com>
2018-05-15 11:29:51 -0700

    Set font family and size

--- src/renderer/dom/DomRenderer.ts
+++ src/renderer/dom/DomRenderer.ts
@@ -136,6 +136,8 @@ export class DomRenderer extends EventEmitter implements IRenderer {
         `${this._terminalSelector} .${ROW_CONTAINER_CLASS} {` +
         ` color: ${this.colorManager.colors.foreground.css};` +
         ` background-color: ${this.colorManager.colors.background.css};` +
+        ` font-family: ${this._terminal.getOption('fontFamily')};` +
+        ` font-size: ${this._terminal.getOption('fontSize')}px;` +
         `}`;
     // Text styles
     styles +=
