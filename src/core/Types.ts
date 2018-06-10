b4ccbe042bdd2f8046fe346f99e114b8a533ef2b
Daniel Imms <daimms@microsoft.com>
2018-06-10 11:52:25 +0200

    Pull keyboard event translation into core

---
+++ src/core/Types.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2018 The xterm.js authors. All rights reserved.
+ * @license MIT
+ */
+
+export interface IKeyboardResult {
+  cancel: boolean;
+  key: string;
+  scrollLines: number;
+}
