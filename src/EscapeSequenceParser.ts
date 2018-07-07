87e292414ddb4c73f82f96cfea1b4dab5f91f690
Jörg Breitbart <jerch@rockborn.de>
2018-07-07 12:46:51 +0200

    fix out of bound deopt in EscapeSequenceParser.parse

--- src/EscapeSequenceParser.ts
+++ src/EscapeSequenceParser.ts
@@ -375,8 +375,7 @@ export class EscapeSequenceParser extends Disposable implements IEscapeSequenceP
       // shortcut for most chars (print action)
       if (currentState === ParserState.GROUND && code > 0x1f && code < 0x80) {
         print = (~print) ? print : i;
-        do code = data.charCodeAt(++i);
-        while (i < l && code > 0x1f && code < 0x80);
+        while (++i < l && data.charCodeAt(i) > 0x1f && data.charCodeAt(i) < 0x80) {}
         i--;
         continue;
       }
