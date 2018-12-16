fff33374f457629ee1df0daba0189b9d0a211563
Jörg Breitbart <jerch@rockborn.de>
2018-12-16 01:04:30 +0100

    fix tests

--- src/Buffer.test.ts
+++ src/Buffer.test.ts
@@ -374,7 +374,7 @@ describe('Buffer', () => {
       terminal.writeSync(input);
       const s = terminal.buffer.iterator(true).next().content;
       // FIXME: currently one space short due to wrong trimming in stringIndexToBufferIndex
-      assert.equal('Sitting in the cafe\u0301drinking coffee.', s);
+      assert.equal('Sitting in the cafe\u0301 drinking coffee.', s);
       for (let i = 0; i < 19; ++i) {
         const bufferIndex = terminal.buffer.stringIndexToBufferIndex(0, i);
         assert.deepEqual([(i / terminal.cols) | 0, i % terminal.cols], bufferIndex);
@@ -440,7 +440,7 @@ describe('Buffer', () => {
       terminal.writeSync(input);
       const s = terminal.buffer.iterator(true).next().content;
       // FIXME: currently one space short due to wrong trimming in stringIndexToBufferIndex
-      assert.equal('𓂀\u0301 - the eye hiroglyph with anacute accent.', s);
+      assert.equal('𓂀\u0301 - the eye hiroglyph with an acute accent.', s);
       // index 0..2 should map to 0
       assert.deepEqual([0, 0], terminal.buffer.stringIndexToBufferIndex(0, 1));
       assert.deepEqual([0, 0], terminal.buffer.stringIndexToBufferIndex(0, 2));
