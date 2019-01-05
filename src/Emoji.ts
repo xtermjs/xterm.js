48e02ab05d918b499e9624560f8f14a74be58cdd
Juan Campa <juancampa@gmail.com>
2019-01-05 13:12:04 -0500

    Short-circuit case for isEmoji.

--- src/Emoji.ts
+++ src/Emoji.ts
@@ -1,11 +1,19 @@
 // This function returns whether the first code point of the provided string is
 // within a range that contains emoji. Some of the codepoints in these ranges
 // are unassigned so this test is an approximation to the real thing.
-// Ranges taken from: https://stackoverflow.com/questions/30757193/
+// Ranges taken from:
+//   https://stackoverflow.com/questions/30757193/
 // For a complete list of all assigned codepoints, go to:
 //   https://unicode.org/Public/emoji/11.0/emoji-data.txt
 export function isEmoji(str: string): boolean {
+  if (str.codePointAt === undefined) {
+    return false;
+  }
   const codePoint = str.codePointAt(0);
+  // Short circuit for the most common case (i.e. non-emoji characters)
+  if (codePoint < 8400) {
+    return false;
+  }
   if ((codePoint >= 0x1F600 && codePoint <= 0x1F64F) || // Emoticons
       (codePoint >= 0x1F300 && codePoint <= 0x1F5FF) || // Misc Symbols and Pictographs
       (codePoint >= 0x1F680 && codePoint <= 0x1F6FF) || // Transport and Map
