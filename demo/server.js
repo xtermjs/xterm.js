58d461bb9047c9282e627a5ea323fef863112ee2
Jörg Breitbart <jerch@rockborn.de>
2019-05-13 15:33:54 +0200

    optimize Buffer.concat

--- demo/server.js
+++ demo/server.js
@@ -92,13 +92,16 @@ function startServer() {
     function bufferUtf8(socket, timeout) {
       let buffer = [];
       let sender = null;
+      let length = 0;
       return (data) => {
         buffer.push(data);
+        length += data.length;
         if (!sender) {
           sender = setTimeout(() => {
-            socket.send(Buffer.concat(buffer));
+            socket.send(Buffer.concat(buffer, length));
             buffer = [];
             sender = null;
+            length = 0;
           }, timeout);
         }
       };
