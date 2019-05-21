0b30ea636d872e529dde25794416e5c853f303e3
Daniel Imms <daimms@microsoft.com>
2019-05-21 11:36:16 -0700

    Fix DSR
    Fixes #2107

--- src/Terminal.ts
+++ src/Terminal.ts
@@ -350,6 +350,7 @@ export class Terminal extends EventEmitter implements ITerminal, IDisposable, II
     this._inputHandler = new InputHandler(this);
     this._inputHandler.onCursorMove(() => this._onCursorMove.fire());
     this._inputHandler.onLineFeed(() => this._onLineFeed.fire());
+    this._inputHandler.onData(e => this._onData.fire(e));
     this.register(this._inputHandler);
 
     // Reuse renderer if the Terminal is being recreated via a reset call.
