eb6e3ab7f75974b50c8e98fe017eae1f2f503fe3
Daniel Imms <daimms@microsoft.com>
2019-05-26 12:43:33 -0700

    Fix tests on Windows (hopefully)

---
+++ bin/test.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2019 The xterm.js authors. All rights reserved.
+ * @license MIT
+ */
+
+const cp = require('child_process');
+const path = require('path');
+
+// Add `out` to the NODE_PATH so absolute paths can be resolved.
+const env = { ...process.env };
+env.NODE_PATH = path.resolve(__dirname, '../out');
+
+cp.spawnSync('yarn', ['mocha'], {
+  cwd: path.join(__dirname, '..'),
+  stdio: 'inherit',
+  env
+});
