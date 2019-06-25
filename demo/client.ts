5459c48525c63e142f84e9250064738502486df3
Daniel Imms <tyriar@tyriar.com>
2019-06-24 20:20:08 -0700

    Add wordSeparator to demo

--- demo/client.ts
+++ demo/client.ts
@@ -235,7 +235,8 @@ function initOptions(term: TerminalType): void {
     fontFamily: null,
     fontWeight: ['normal', 'bold', '100', '200', '300', '400', '500', '600', '700', '800', '900'],
     fontWeightBold: ['normal', 'bold', '100', '200', '300', '400', '500', '600', '700', '800', '900'],
-    rendererType: ['dom', 'canvas']
+    rendererType: ['dom', 'canvas'],
+    wordSeparator: null
   };
   const options = Object.keys((<any>term)._core.options);
   const booleanOptions = [];
