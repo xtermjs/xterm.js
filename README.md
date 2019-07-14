8359605951524d93580e073e8e2cf397d0505193
Daniel Imms <daimms@microsoft.com>
2019-07-13 19:54:01 -0700

    Add license and credit to VS Code

--- README.md
+++ README.md
@@ -185,3 +185,5 @@ If you contribute code to this project, you are implicitly allowing your code to
 Copyright (c) 2017-2019, [The xterm.js authors](https://github.com/xtermjs/xterm.js/graphs/contributors) (MIT License)<br>
 Copyright (c) 2014-2017, SourceLair, Private Company ([www.sourcelair.com](https://www.sourcelair.com/home)) (MIT License)<br>
 Copyright (c) 2012-2013, Christopher Jeffrey (MIT License)
+
+Some files in this code base are heavily influenced on implementations in [Visual Studio Code](https://github.com/Microsoft/vscode) (MIT License).
--- src/common/services/ServiceRegistry.ts
+++ src/common/services/ServiceRegistry.ts
@@ -1,7 +1,13 @@
 /**
  * Copyright (c) 2019 The xterm.js authors. All rights reserved.
  * @license MIT
+ *
+ * This was heavily inspired from microsoft/vscode's dependency injection system (MIT).
  */
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 import { IServiceIdentifier } from 'common/services/Services';
 
