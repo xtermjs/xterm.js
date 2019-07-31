bbd7275ad727e4f9bb8963172b3c4b7d87f3306e
Jörg Breitbart <jerch@rockborn.de>
2019-07-31 14:44:50 +0200

    make linter happy

--- src/InputHandler.ts
+++ src/InputHandler.ts
@@ -231,7 +231,7 @@ export class InputHandler extends Disposable implements IInputHandler {
     this._parser.setExecuteHandler(C0.SO, () => this.shiftOut());
     this._parser.setExecuteHandler(C0.SI, () => this.shiftIn());
     // FIXME:   What do to with missing? Old code just added those to print.
-    
+
     this._parser.setExecuteHandler(C1.IND, () => this.index());
     this._parser.setExecuteHandler(C1.NEL, () => this.nextLine());
     this._parser.setExecuteHandler(C1.HTS, () => this.tabSet());
