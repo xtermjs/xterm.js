dac2da05bf1d3ccdc19f17296e49d6c53ff612d7
Daniel <tyriar@tyriar.com>
2019-10-25 04:13:03 -0700

    Clarify marker API docs

--- typings/xterm.d.ts
+++ typings/xterm.d.ts
@@ -325,7 +325,8 @@ declare module 'xterm' {
 
   /**
    * Represents a specific line in the terminal that is tracked when scrollback
-   * is trimmed and lines are added or removed.
+   * is trimmed and lines are added or removed. This is a single line that may
+   * be part of a larger wrapped line.
    */
   export interface IMarker extends IDisposable {
     /**
@@ -339,7 +340,8 @@ declare module 'xterm' {
     readonly isDisposed: boolean;
 
     /**
-     * The actual line index in the buffer at this point in time.
+     * The actual line index in the buffer at this point in time. This is set to
+     * -1 if the marker has been disposed.
      */
     readonly line: number;
   }
