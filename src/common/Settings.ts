f088235ac262cf112354e127bffc561477072f3f
Jörg Breitbart <jerch@rockborn.de>
2019-11-05 13:50:29 +0100

    more interfaces, Settings class stub

--- src/common/Settings.ts
+++ src/common/Settings.ts
@@ -1,52 +1,200 @@
 /**
- * Base type for any settings option.
- * Only meant for primitive types T = string | number | boolean.
- * 
- * TODO: make this actually useful with a good abstraction for different option needs
+ * Some helpers for mapped types.
  */
-interface IOption<T> {
-  init: T;
-  // whether this option is directly writeable (always false if getter/setter is defined)
-  writeable?: boolean;
-  // optional getter/setter of this value
-  // needs some clever mapping in Settings which internal cheap access later on
-  getter?: () => T;
-  setter?: (v: T) => void;
-}
+// return partial type X satisfying type condition Y
+type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B;
 
-// dummy needed to separate type from generic object type
-// NOTE: This must not contain an index declaration or DeepRealisation will fail.
-interface IOptionCollection { }
+// filter option vs. option(writeable/readonly) vs. collection type
+type IsOption<T> = T extends IOption<any, boolean> ? T : never;
+type IsWriteable<T> = T extends IOption<any, true> ? never: T;
+type IsReadonly<T> = T extends IOption<any, true> ? T: never;
+type IsCollection<T> = T extends IOption<any, boolean> ? never : T;
+
+// writeable option keys from collection
+type GetWriteableOptionKeys<T extends IOptionCollection> = {
+  [P in keyof T]: IfEquals<{ [Q in P]: T[P] }, { [Q in P]: IsWriteable<IsOption<T[P]>> }, P>
+}[keyof T];
+
+// readonly option keys from collection
+type GetReadonlyOptionKeys<T extends IOptionCollection> = {
+  [P in keyof T]: IfEquals<{ [Q in P]: T[P] }, { [Q in P]: IsReadonly<IsOption<T[P]>> }, P>
+}[keyof T];
+
+// collection keys from collection
+type GetCollectionKeys<T extends IOptionCollection> = {
+  [P in keyof T]: IfEquals<{ [Q in P]: T[P] }, { [Q in P]: IsCollection<T[P]> }, P>
+}[keyof T];
+
+// prettify intersection type
+type Prettify<T> = T extends object ? {[K in keyof T]: T[K]} : T;
+
+// add index
+type AddIndex<T, U> = T & {[key: string]: U};
+
 
 /**
- * Create a realisation type from an IOptionCollection with these characteristics:
- *    - properties are casted: IOption<T> ==> T
- *    - all properties are made optional
- *    - unknown properties are rejected
- * 
- * This type is useful to create a settings object which contains only
- * certain properties for setting overwrites.
+ * IOption is limited to these primitive types.
  */
-type DeepRealisation<T> = T extends IOption<number> ? number
-  : T extends IOption<boolean> ? boolean
-  : T extends IOption<string> ? string
-  : T extends IOptionCollection ? { [K in keyof T]?: DeepRealisation<T[K]> } : never;
+type AllowedOptionTypes = string | number | boolean;
 
+/**
+ * Base type for any settings option.
+ * Used to create an option entry in an IOptionCollection.
+ */
+interface IOption<T extends AllowedOptionTypes, U extends boolean = false> {
+  /** default value after initialization */
+  init: T;
+  /** whether this option is readonly during runtime */
+  readonly: U;
+  /** TODO: get/set */
+  getter?: () => T;
+  setter?: (v: T) => void;
+  /** filter function to be applied when retrieving an option from settings */
+  fromSettings?: (v: T) => T;
+  /** filter function to be applied when setting an option in settings */
+  toSettings?: (v: T) => T;
+}
+
+/**
+ * Collection type for IOptions.
+ * Used to create a settings definition that are turned into a settings object
+ * by a later `settingsFactory` call.
+ * For proper type inference always extend option collections from this interface.
+ * The collection may not contain other entries than IOption<T, U> or other
+ * IOptionCollection as sub groups.
+ */
+interface IOptionCollection { }
+type IndexedOptionCollection<T extends IOptionCollection> = AddIndex<T, IOption<AllowedOptionTypes, boolean> | IOptionCollection>;
+
+
+/**
+ * InitialOptions<T>
+ * Contains all options as writeable/optional to allow partial initialization
+ * with custom values.
+ */
+type InitialOptions<T> = T extends IOption<infer O, boolean> ? O
+  : T extends IOptionCollection ? { [K in keyof T]?: InitialOptions<T[K]> }
+  : never;
+
+/**
+ * ISettings<T> - interface of the settings object
+ * Contains all options including readonly properties.
+ * Used as read interface of `Settings`.
+ */
+type ISettings<T> = T extends IOption<infer O, boolean> ? O
+  : T extends IOptionCollection ?
+    Prettify<{ readonly [K in keyof Pick<T, GetReadonlyOptionKeys<T>>]: ISettings<T[K]>; }
+     & { -readonly [K in keyof Pick<T, GetCollectionKeys<T> | GetWriteableOptionKeys<T>>]: ISettings<T[K]>; }
+     & ISettingsBase<T>>
+  : never;
+
+/**
+ * WriteableOptions<T>
+ * Contains all writeable options as optional to allow partial assignments during runtime.
+ * Used as write interface of `Settings`.
+ */
+type WriteableOptions<T> = T extends IOption<infer O, boolean> ? O
+: T extends IOptionCollection ? {
+    -readonly [K in keyof Pick<T, GetCollectionKeys<T> | GetWriteableOptionKeys<T>>]?: WriteableOptions<T[K]>;
+  }
+: never;
+
+interface ISettingsBase<T extends IOptionCollection> {
+  asWriteable: WriteableOptions<T>;
+}
+
+/**
+ * Settings class
+ *
+ * Class for maintaining global settings.
+ * Note: Do not use the ctor directly, instead use `createSettings`
+ * for correct type inference.
+ *
+ * Characteristics:
+ * - recursive for nested IOptionCollections
+ * - stable, will never change identity
+ * - mutability only on IOption level
+ * - read interface by default
+ * - write interface with `asWriteable`
+ *
+ * TODO:
+ * - shim for old style access with getOption / setOption
+ * - customizable getter / setter / toSettings / fromSettings
+ */
+
+
+class Settings<T extends IOptionCollection, K extends keyof T> {
+  constructor(definition: T, initials?: InitialOptions<T>) {
+    const def = definition as IndexedOptionCollection<T>;
+    for (const optionName in definition) {
+      const option = def[optionName];
+      const init = initials as AddIndex<InitialOptions<T>, number | string | boolean | object>;
+      if (option.hasOwnProperty('init')) {
+        // IOption
+        const op = option as IOption<AllowedOptionTypes, boolean>;
+        const v = init && init[optionName] !== undefined ? init[optionName] : op.init;
+        Object.defineProperty(this, optionName, {
+          value: v,
+          writable: !op.readonly,
+          enumerable: true
+        });
+      } else {
+        // IOptionCollection
+        const op = option as any;
+        const subSettings: Settings<T[K], keyof T[K]> = new Settings(op, init ? init[optionName] : undefined);
+        Object.defineProperty(this, optionName, {
+          enumerable: true,
+          get(): Settings<T[K], keyof T[K]> {
+            return subSettings;
+          },
+          set(values: WriteableOptions<T[K]>): void {
+            for (const v in values) {
+              if (subSettings.hasOwnProperty(optionName)) {
+                (subSettings as any)[v] = values[v] as any;
+              }
+            }
+          }
+        });
+      }
+    }
+  }
+  // writeable options (setter only)
+  set asWriteable(value: WriteableOptions<T>) {
+    for (const optionName in value) {
+      if (this.hasOwnProperty(optionName)) {
+        (this as any)[optionName] = value[optionName];
+      }
+    }
+  }
+}
+
+/**
+ * Create a settings object from a definition and optional initial values.
+ * Use this factory function to ensure correct type inference.
+ *
+ * @param definition option definitions
+ * @param initial initial values
+ */
+function createSettings<T extends IOptionCollection>(definition: T, initial?: InitialOptions<T>): ISettings<T> {
+  return new Settings(definition, initial) as ISettings<T>;
+}
 
 /**
  * Test examples.
  */
 
-// interfaces - always derive option collections from IOptionCollection
+// example settings interfaces
+// NOTE: Always extend collections from IOptionCollection
 interface ITerminalOptions extends IOptionCollection {
-  cols: IOption<number>;
+  /** This should appear as help text for T.cols on all derived types T */
+  cols: IOption<number, true>;
   rows: IOption<number>;
   parser: IParserOptions;
 }
 
 interface IParserOptions extends IOptionCollection {
   x: IOption<boolean>;
-  y: IOption<string>;
+  y: IOption<'red' | 'green' | 'blue', true>;
   windowOptions: IWindowOptions;
 }
 
@@ -57,13 +205,13 @@ interface IWindowOptions extends IOptionCollection {
 // initial definitions with defaults
 // can be defined all in once
 const ALL_IN_ONCE: ITerminalOptions = {
-  cols: {init: 80},
-  rows: {init: 25},
+  cols: {init: 80, readonly: true},
+  rows: {init: 25, readonly: false},
   parser: {
-      x: {init: true},
-      y: {init: 'hello'},
+      x: {init: true, readonly: false},
+      y: {init: 'red', readonly: true},
       windowOptions: {
-          resizeWin: {init: false}
+          resizeWin: {init: false, readonly: false}
         }
   }
 };
@@ -71,57 +219,57 @@ const ALL_IN_ONCE: ITerminalOptions = {
 
 // or spread across separate definitions
 const W_DEFAULT: IWindowOptions = {
-  resizeWin: {init: false}
+  resizeWin: {init: false, readonly: false}
 };
 
 const P_DEFAULT: IParserOptions = {
-  x: {init: true},
-  y: {init: 'yo'},
+  x: {init: true, readonly: false},
+  y: {init: 'green', readonly: true},
   windowOptions: W_DEFAULT
 };
 
 // and joined later on
 const T_DEFAULT: ITerminalOptions = {
-  cols: {init: 80},
-  rows: {init: 25},
+  cols: {init: 80, readonly: true},
+  rows: {init: 25, readonly: false},
   parser: P_DEFAULT
-}
+};
 
 
-/**
- * Test realisation type
- */
-
-// some settings shall be changed
+// some settings shall be changed on init
 // Note: all values are correctly inferred from the definition above
-const customizeValues: DeepRealisation<ITerminalOptions> = {
+const customizedInitialValues: InitialOptions<ITerminalOptions> = {
   cols: 12,
   rows: 88,
   parser: {
     x: false,
-    y: 'yo',
+    y: 'blue',
     windowOptions: {
-      resizeWin: true,
+      resizeWin: true
     }
   }
 };
 
 
+declare const console: any;
 
-/**
- * Settings class...TODO
- * 
- * This is the final deal with these characteristics:
- * - recursive Settings for IOptionCollections
- * - object is stable - will never change identity
- * - mutability only for primitves string | number | boolean
- * - allow bootstrap of settings from definitions with optional overwrites:
- *    `new Settings(definition: SomeIOptionCollection, overwrites: DeepRealisation<SomeIOptionCollection>)`
- * - allow partial settings updates
- *    `settings.xy = z`
- *    `settings.parser = {y: 'yo', windowOptions: {resizeWin: false}}`
- * - set/getOption for old style access
- */
-class Settings {
 
-}
+const settings = createSettings(ALL_IN_ONCE, customizedInitialValues);
+console.log(settings);
+settings.rows = 6;
+// readonly properties cannot be assigned later on
+// ss.cols = 7;
+
+// identity check
+const savedWindowOptions = settings.parser.windowOptions;
+console.log(settings.parser, settings.parser.windowOptions);
+
+// partial re-assignments through write interface
+settings.parser.asWriteable = {x: true, windowOptions: {resizeWin: false}};
+console.log(settings.parser, settings.parser.windowOptions);
+
+// write interface cannot be read
+console.log('read writeable:', settings.asWriteable);
+
+// should always be true
+console.log(savedWindowOptions === settings.parser.windowOptions);
