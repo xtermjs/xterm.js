840e56cf1e0e53cab83b011f4455f1e62030e134
Daniel Imms <daimms@microsoft.com>
2019-11-09 11:30:45 -0800

    Fix Attach tests

--- addons/xterm-addon-attach/src/AttachAddon.api.ts
+++ addons/xterm-addon-attach/src/AttachAddon.api.ts
@@ -20,7 +20,6 @@ describe('AttachAddon', () => {
     this.timeout(20000);
     browser = await puppeteer.launch({
       headless: process.argv.indexOf('--headless') !== -1,
-      slowMo: 0,
       args: [`--window-size=${width},${height}`, `--no-sandbox`]
     });
     page = (await browser.pages())[0];
@@ -43,6 +42,7 @@ describe('AttachAddon', () => {
     const server = new WebSocket.Server({ port });
     server.on('connection', socket => socket.send('foo'));
     await page.evaluate(`window.term.loadAddon(new window.AttachAddon(new WebSocket('ws://localhost:${port}')))`);
+    await pollFor(page, `window.term.buffer.getLine(0).translateToString(true)`, 'foo');
     assert.equal(await page.evaluate(`window.term.buffer.getLine(0).translateToString(true)`), 'foo');
     server.close();
   });
@@ -55,6 +55,7 @@ describe('AttachAddon', () => {
     const data = new Uint8Array([102, 111, 111]);
     server.on('connection', socket => socket.send(data));
     await page.evaluate(`window.term.loadAddon(new window.AttachAddon(new WebSocket('ws://localhost:${port}')))`);
+    await pollFor(page, `window.term.buffer.getLine(0).translateToString(true)`, 'foo');
     assert.equal(await page.evaluate(`window.term.buffer.getLine(0).translateToString(true)`), 'foo');
     server.close();
   });
@@ -67,5 +68,14 @@ async function openTerminal(options: ITerminalOptions = {}): Promise<void> {
     await page.waitForSelector('.xterm-rows');
   } else {
     await page.waitForSelector('.xterm-text-layer');
+  }
+}
+
+async function pollFor(page: puppeteer.Page, fn: string, val: any): Promise<void> {
+  const result = await page.evaluate(fn);
+  if (result !== val) {
+    return new Promise<void>(r => {
+      setTimeout(() => r(pollFor(page, fn, val)), 10);
+    });
   }
 }
