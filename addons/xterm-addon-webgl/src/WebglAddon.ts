bcd4c766d61bbaf4d275d7faa1994d72923acdf5
Daniel Imms <daimms@microsoft.com>
2019-11-09 09:09:59 -0800

    Implement webgl renderer dispose
    Fixes #2254

--- addons/xterm-addon-webgl/src/WebglAddon.ts
+++ addons/xterm-addon-webgl/src/WebglAddon.ts
@@ -9,20 +9,28 @@ import { IRenderService } from 'browser/services/Services';
 import { IColorSet } from 'browser/Types';
 
 export class WebglAddon implements ITerminalAddon {
+  private _terminal?: Terminal;
+
   constructor(
     private _preserveDrawingBuffer?: boolean
   ) {}
 
   public activate(terminal: Terminal): void {
     if (!terminal.element) {
-      throw new Error('Cannot activate WebglRendererAddon before Terminal.open');
+      throw new Error('Cannot activate WebglAddon before Terminal.open');
     }
+    this._terminal = terminal;
     const renderService: IRenderService =  (<any>terminal)._core._renderService;
     const colors: IColorSet = (<any>terminal)._core._colorManager.colors;
     renderService.setRenderer(new WebglRenderer(terminal, colors, this._preserveDrawingBuffer));
   }
 
   public dispose(): void {
-    throw new Error('WebglRendererAddon.dispose Not yet implemented');
+    if (!this._terminal) {
+      throw new Error('Cannot dispose WebglAddon because it is activated');
+    }
+    const renderService: IRenderService = (<any>this._terminal)._core._renderService;
+    renderService.setRenderer((<any>this._terminal)._core._createRenderer());
+    renderService.onResize(this._terminal.cols, this._terminal.rows);
   }
 }
