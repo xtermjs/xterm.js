d7737f6c885d5ec5a2f1a8d836cbf0e6dd4a9ceb
Jörg Breitbart <jerch@rockborn.de>
2019-12-07 13:39:06 +0100

    cleanup

--- src/InputHandler.ts
+++ src/InputHandler.ts
@@ -44,28 +44,28 @@ function paramToWindowOption(n: number, opts: IWindowOptions): boolean {
     return opts.setWinLines || false;
   }
   switch (n) {
-    case 1: return opts.restoreWin || false;
-    case 2: return opts.minimizeWin || false;
-    case 3: return opts.setWinPosition || false;
-    case 4: return opts.setWinSizePixels || false;
-    case 5: return opts.raiseWin || false;
-    case 6: return opts.lowerWin || false;
-    case 7: return opts.refreshWin || false;
-    case 8: return opts.setWinSizeChars || false;
-    case 9: return opts.maximizeWin || false;
-    case 10: return opts.fullscreenWin || false;
-    case 11: return opts.getWinState || false;
-    case 13: return opts.getWinPosition || false;
-    case 14: return opts.getWinSizePixels || false;
-    case 15: return opts.getScreenSizePixels || false;
-    case 16: return opts.getCellSizePixels || false;
-    case 18: return opts.getWinSizeChars || false;
-    case 19: return opts.getScreenSizeChars || false;
-    case 20: return opts.getIconTitle || false;
-    case 21: return opts.getWinTitle || false;
-    case 22: return opts.pushTitle || false;
-    case 23: return opts.popTitle || false;
-    case 24: return opts.setWinLines || false;
+    case 1: return !!opts.restoreWin;
+    case 2: return !!opts.minimizeWin;
+    case 3: return !!opts.setWinPosition;
+    case 4: return !!opts.setWinSizePixels;
+    case 5: return !!opts.raiseWin;
+    case 6: return !!opts.lowerWin;
+    case 7: return !!opts.refreshWin;
+    case 8: return !!opts.setWinSizeChars;
+    case 9: return !!opts.maximizeWin;
+    case 10: return !!opts.fullscreenWin;
+    case 11: return !!opts.getWinState;
+    case 13: return !!opts.getWinPosition;
+    case 14: return !!opts.getWinSizePixels;
+    case 15: return !!opts.getScreenSizePixels;
+    case 16: return !!opts.getCellSizePixels;
+    case 18: return !!opts.getWinSizeChars;
+    case 19: return !!opts.getScreenSizeChars;
+    case 20: return !!opts.getIconTitle;
+    case 21: return !!opts.getWinTitle;
+    case 22: return !!opts.pushTitle;
+    case 23: return !!opts.popTitle;
+    case 24: return !!opts.setWinLines;
   }
   return false;
 }
