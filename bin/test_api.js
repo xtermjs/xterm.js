1f4b2607cca815ffb72f3e6e37af06202c6facc2
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-04-13 22:36:33 +0530

    Replace flatMap method with reduce and map

--- bin/test_api.js
+++ bin/test_api.js
@@ -21,7 +21,7 @@ let flagArgs = [];
 
 if (process.argv.length > 2) {
   const args = process.argv.slice(2);
-  flagArgs = args.filter(e => e.startsWith('--')).flatMap(arg => arg.split('='));
+  flagArgs = args.filter(e => e.startsWith('--')).map(arg => arg.split('=')).reduce((arr, val) => arr.concat([...val], []));
   console.info(flagArgs);
   // ability to inject particular test files via
   // yarn test [testFileA testFileB ...]
