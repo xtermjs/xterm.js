20f320327c40096ac752a571f58f9aff83eea066
meganrogge <megrogge@gmail.com>
2021-05-05 19:45:36 -0700

    Tweak range

--- src/common/buffer/BufferRange.ts
+++ src/common/buffer/BufferRange.ts
@@ -7,7 +7,7 @@ import { IBufferRange } from 'xterm';
 
 export function getRangeLength(range: IBufferRange, cols: number): number {
   if (range.start.y === range.end.y) {
-    return range.end.x - range.start.x;
+    return range.end.x - range.start.x + 1;
   }
   if (range.start.y > range.end.y) {
     throw new Error(`Buffer range end (${range.end.x}, ${range.end.y}) cannot be before start (${range.start.x}, ${range.start.y})`);
