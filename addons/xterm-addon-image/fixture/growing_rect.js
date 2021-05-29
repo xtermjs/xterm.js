c98eb8f7faaa76e11e7a811445a2fa93a94486e6
Jörg Breitbart <jerch@rockborn.de>
2021-05-29 11:09:49 +0200

    growing rectangle test without flickering

--- addons/xterm-addon-image/fixture/growing_rect.js
+++ addons/xterm-addon-image/fixture/growing_rect.js
@@ -7,25 +7,38 @@ function createRect(size, color) {
   return sixelEncode(new Uint8ClampedArray(pixels.buffer), size, size);
 }
 
+function createRectMinusOne(size, color) {
+  const pixels = new Uint32Array(size * size);
+  if (size - 1) {
+    const sub = new Uint32Array(size - 1);
+    sub.fill(toRGBA8888(...color));
+    const last = size * (size - 1);
+    for (let y = 0; y < last; y += size) {
+      pixels.set(sub, y);
+    }
+  }
+  return sixelEncode(new Uint8ClampedArray(pixels.buffer), size, size);
+}
+
 async function main() {
   // clear + cursor and sixelScrolling off
   process.stdout.write('\x1b[2J\x1b[?25;80l');
 
   for (let i = 1; i < 300; ++i) {
     await new Promise(res => setTimeout(() => {
-      process.stdout.write('\x1b[2J' + createRect(i, [0, 255, 0]));
+      process.stdout.write(createRect(i, [0, 255, 0]));
       res();
-    }, 10));
+    }, 5));
   }
   for (let i = 299; i >= 1; --i) {
     await new Promise(res => setTimeout(() => {
-      process.stdout.write('\x1b[2J' + createRect(i, [0, 255, 0]));
+      process.stdout.write(createRectMinusOne(i, [0, 255, 0]));
       res();
-    }, 10));
+    }, 5));
   }
 
   // re-enable cursor and sixel scrolling
-  process.stdout.write('\x1b[?25;80h');
+  process.stdout.write('\x1b[2J\x1b[?25;80h');
 }
 
 main();
