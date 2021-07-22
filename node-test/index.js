40992a7f76d1732c0bf67b7ad3f7e579aad385b3
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-07-22 16:18:07 -0700

    Update node test

--- node-test/index.js
+++ node-test/index.js
@@ -1,12 +1,12 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const xterm = require('xterm-core');
+const Terminal = require('../lib-headless/xterm.js').Terminal;
 
 console.log('Creating xterm-core terminal...');
-const terminal = new xterm.Terminal();
+const terminal = new Terminal();
 console.log('Writing `ls` to terminal...')
-terminal.write('ls', () => {
+terminal.write('foo \x1b[1;31mbar\x1b[0m baz', () => {
   const bufferLine = terminal.buffer.normal.getLine(terminal.buffer.normal.cursorY);
-  const contents = bufferLine.translateToString();
-  console.log(`Contents of terminal active buffer are: ${contents}`); // ls
+  const contents = bufferLine.translateToString(true);
+  console.log(`Contents of terminal active buffer are: ${contents}`); // foo bar baz
 });
