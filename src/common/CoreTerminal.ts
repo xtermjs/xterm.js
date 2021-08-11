57c7e6af4703cd4040da36baa36bd3f47a166e14
meganrogge <megrogge@gmail.com>
2021-08-11 15:19:12 -0700

    add more characters

--- src/common/CoreTerminal.ts
+++ src/common/CoreTerminal.ts
@@ -149,7 +149,12 @@ export abstract class CoreTerminal extends Disposable implements ICoreTerminal {
       // this.write('▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇');
       this.write('━━│┃\r\n');
       this.write('                                ━━│┃┌┍┎┏\r\n');
-      this.write('                                ━━│┃┌┐┍┑┎┒┏┓');
+      this.write('                                ━━│┃┌┐┍┑┎┒┏┓\r\n');
+      this.write('                                    └┘┕┙┖┚┗┛\r\n');
+      this.write('├┝┞┟┠┡┢┣┤┥┦┧┨┩┪┫┬┭┮┯┰┱┲┳┴┵┶┷┸┹┺┻┼┽┾┿╀╁╂╃╄╅╆╇╈╉╊╋╌╍╎╏═║╒╓╔╕╖╗╘╙╚╛╜╝╞╟╠╡╢╣╤╥╦╧╨╩╪╫╬╭╮╯╰╱╲╳╴╵╶╷╸╹╺╻╼╽╾╿\r\n');
+      this.write('├ ┝ ┞ ┟ ┠ ┡ ┢ ┣ ┤ ┥ ┦ ┧ ┨ ┩ ┪ ┫ ┬ ┭ ┮ ┯ ┰ ┱ ┲ ┳ ┴ ┵ ┶ ┷ ┸ ┹ ┺ ┻\r\n');
+      this.write('┼ ┽ ┾ ┿ ╀ ╁ ╂ ╃ ╄ ╅ ╆ ╇ ╈ ╉ ╊ ╋\r\n');
+      this.write('╌ ╍ ╎ ╏ ═ ║ ╒ ╓ ╔ ╕ ╖ ╗ ╘ ╙ ╚ ╛ ╜ ╝ ╞ ╟ ╠ ╡ ╢ ╣ ╤ ╥ ╦ ╧ ╨ ╩ ╪ ╫ ╬ ╭ ╮ ╯ ╰ ╱ ╲ ╳ ╴ ╵ ╶ ╷ ╸ ╹ ╺ ╻ ╼ ╽ ╾ ╿\r\n');
     }, 1000);
   }
 
