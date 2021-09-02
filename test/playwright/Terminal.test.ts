bee19ec68b2ceea16b9bb3aacf8aa1f2e2fc7d33
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-09-02 09:44:11 -0700

    Make openTerminal optional when default options are used

--- test/playwright/Terminal.test.ts
+++ test/playwright/Terminal.test.ts
@@ -1,13 +1,14 @@
-import { Page, test } from '@playwright/test';
-import { fail, ok, strictEqual, throws } from 'assert';
+import { test } from '@playwright/test';
+import { strictEqual } from 'assert';
 import { createTextContext as createTestContext, ITestContext, openTerminal, asyncThrows } from './TestUtils';
 
 let ctx: ITestContext;
 test.beforeAll(async ({ browser }) => ctx = await createTestContext(browser));
 
 test.describe.serial('API integration tests', () => {
+  test.beforeEach(async () => await openTerminal(ctx));
+
   test('Default options', async () => {
-    await openTerminal(ctx);
     await ctx.page.evaluate(([term]) => term.cols, [ctx.termHandle]);
     strictEqual(await ctx.proxy.cols, 80);
     strictEqual(await ctx.proxy.rows, 24);
@@ -15,6 +16,6 @@ test.describe.serial('API integration tests', () => {
 
   test('Proposed API check', async () => {
     await openTerminal(ctx, { allowProposedApi: false });
-    await asyncThrows(() => ctx.proxy.evaluate(([term]) => term.buffer), 'You must set the allowProposedApi option to true to use proposed API');
+    await asyncThrows(async () => await ctx.proxy.evaluate(([term]) => term.buffer), 'You must set the allowProposedApi option to true to use proposed API');
   });
 });
