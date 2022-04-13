cfbb1b2ee2d84b2670a92cd6d529c29b0a69f18f
meganrogge <merogge@microsoft.com>
2022-04-13 10:47:10 -0700

    update overview ruler decoration even when data hasn't changed

--- addons/xterm-addon-search/src/SearchAddon.ts
+++ addons/xterm-addon-search/src/SearchAddon.ts
@@ -713,7 +713,7 @@ export class SearchAddon implements ITerminalAddon {
       marker,
       x: result.col,
       width: result.size,
-      overviewRulerOptions: this._resultDecorations?.get(marker.line) && !this._dataChanged ? undefined : {
+      overviewRulerOptions: this._resultDecorations?.get(marker.line) ? undefined : {
         color: decorations.matchOverviewRuler, position: 'center'
       }
     });
