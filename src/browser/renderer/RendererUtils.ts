da33543ffb96f43e1904bc335d8cacd4eed407d6
Megan Rogge <merogge@microsoft.com>
2022-04-20 14:25:46 -0700

    align isPowerlineGlyph across renderers (#3743)

--- src/browser/renderer/RendererUtils.ts
+++ src/browser/renderer/RendererUtils.ts
@@ -9,3 +9,9 @@ export function throwIfFalsy<T>(value: T | undefined | null): T {
   }
   return value;
 }
+
+export function isPowerlineGlyph(codepoint: number): boolean {
+  // This range was established via
+  // https://apw-bash-settings.readthedocs.io/en/latest/fontpatching.html
+  return 0xE000 <= codepoint && codepoint <=  0xF8FF;
+}
