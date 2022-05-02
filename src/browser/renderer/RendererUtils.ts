953c67d13018ba26d76a4126e556812ba45ce9b1
meganrogge <merogge@microsoft.com>
2022-05-02 13:37:46 -0700

    add a comment

--- src/browser/renderer/RendererUtils.ts
+++ src/browser/renderer/RendererUtils.ts
@@ -11,5 +11,8 @@ export function throwIfFalsy<T>(value: T | undefined | null): T {
 }
 
 export function isPowerlineGlyph(codepoint: number): boolean {
+  // Only return true for Powerline symbols which require
+  // different padding and should be excluded from minimum contrast
+  // ratio standards
   return 0xE0A0 <= codepoint && codepoint <=  0xE0D6;
 }
