902ea76a1f5a431d09ed98db38c970103f48d122
Megan Rogge <megan.rogge@microsoft.com>
2022-06-01 10:25:29 -0700

    Update src/browser/renderer/RendererUtils.ts
    Co-authored-by: Daniel Imms <2193314+Tyriar@users.noreply.github.com>

--- src/browser/renderer/RendererUtils.ts
+++ src/browser/renderer/RendererUtils.ts
@@ -18,7 +18,7 @@ export function isPowerlineGlyph(codepoint: number): boolean {
 }
 
 function isBoxOrBlockGlyph(codepoint: number): boolean {
-  return (0x2500 <= codepoint && codepoint <= 0x257F) || (0x2580 <= codepoint && codepoint <= 0x259F);
+  return (0x2500 <= codepoint && codepoint <= 0x259F);
 }
 
 export function excludeFromContrastRatioDemands(codepoint: number): boolean {
