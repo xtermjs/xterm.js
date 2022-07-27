62911022ec958e94f4a66fd34833bee231026f03
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-07-26 17:19:48 -0700

    Revert "Retry Terminal access after 500ms"
    This reverts commit 8fb80c31bdb2dca2709c854988e213292fda1905.

--- test/playwright/TestUtils.ts
+++ test/playwright/TestUtils.ts
@@ -296,21 +296,11 @@ class TerminalCoreProxy {
 
 export async function openTerminal(ctx: ITestContext, options: ITerminalOptions = {}): Promise<void> {
   await ctx.page.evaluate(`
-    return new Promise(r => {
-      if ('term' in window) {
-        window.term.dispose();
-      }
-      try {
-        window.term = new Terminal(${JSON.stringify(options)});
-        window.term.open(document.querySelector('#terminal-container'));
-        r();
-      } catch (e) {
-        setTimeout(() => {
-          window.term = new Terminal(${JSON.stringify(options)});
-          window.term.open(document.querySelector('#terminal-container'));
-          r();
-        }, 500);
-      }
+    if ('term' in window) {
+      window.term.dispose();
+    }
+    window.term = new Terminal(${JSON.stringify(options)});
+    window.term.open(document.querySelector('#terminal-container'));
   `);
   if (options.rendererType === 'dom') {
     await ctx.page.waitForSelector('.xterm-rows');
