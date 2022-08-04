f3f6a91f162c724f70a99b9b93e0873eb09355a0
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-08-04 09:57:37 -0700

    Remove node target from ligatures
    This complicates bundling for sandboxed electron a lot, ideally we would only
    rely on browser APIs for font access

--- addons/xterm-addon-ligatures/webpack.config.js
+++ addons/xterm-addon-ligatures/webpack.config.js
@@ -27,20 +27,5 @@ module.exports = {
     library: addonName,
     libraryTarget: 'umd'
   },
-  mode: 'production',
-  externals: {
-    'font-finder': 'font-finder',
-    'stream': 'stream',
-    'os': 'os',
-    'util': 'util'
-  },
-  resolve: {
-    // The ligature modules contains fallbacks for node environments, we never want to browserify them
-    fallback: {
-      stream: false,
-      util: false,
-      os: false,
-      path: false
-    }
-  }
+  mode: 'production'
 };
