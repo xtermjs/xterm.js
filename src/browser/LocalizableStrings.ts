559b6950b2ba4318ca44948350779e5f364a5f58
greenmashimaro <greenmashimaro@gmail.com>
2022-08-23 15:09:52 +0800

    remove unnecessary eslint comments

--- src/browser/LocalizableStrings.ts
+++ src/browser/LocalizableStrings.ts
@@ -3,8 +3,5 @@
  * @license MIT
  */
 
-// eslint-disable-next-line prefer-const
-export let promptLabel = 'Terminal input';
-
-// eslint-disable-next-line prefer-const
-export let tooMuchOutput = 'Too much output to announce, navigate to rows manually to read';
+export const promptLabel = 'Terminal input';
+export const tooMuchOutput = 'Too much output to announce, navigate to rows manually to read';
