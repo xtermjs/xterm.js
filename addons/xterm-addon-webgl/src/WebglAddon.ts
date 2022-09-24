868fc7ec2954b4529b0dc5e80688a753c8955a23
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-09-24 14:44:13 -0700

    Allow loading canvas before open

--- addons/xterm-addon-webgl/src/WebglAddon.ts
+++ addons/xterm-addon-webgl/src/WebglAddon.ts
@@ -25,20 +25,21 @@ export class WebglAddon implements ITerminalAddon {
   ) {}
 
   public activate(terminal: Terminal): void {
+    const core = (terminal as any)._core;
     if (!terminal.element) {
-      (terminal as any)._core.onWillOpen(() => this.activate(terminal));
+      core.onWillOpen(() => this.activate(terminal));
       return;
     }
     if (isSafari) {
       throw new Error('Webgl is not currently supported on Safari');
     }
     this._terminal = terminal;
-    const renderService: IRenderService = (terminal as any)._core._renderService;
-    const characterJoinerService: ICharacterJoinerService = (terminal as any)._core._characterJoinerService;
-    const coreBrowserService: ICoreBrowserService = (terminal as any)._core._coreBrowserService;
-    const coreService: ICoreService = (terminal as any)._core.coreService;
-    const decorationService: IDecorationService = (terminal as any)._core._decorationService;
-    const colors: IColorSet = (terminal as any)._core._colorManager.colors;
+    const renderService: IRenderService = core._renderService;
+    const characterJoinerService: ICharacterJoinerService = core._characterJoinerService;
+    const coreBrowserService: ICoreBrowserService = core._coreBrowserService;
+    const coreService: ICoreService = core.coreService;
+    const decorationService: IDecorationService = core._decorationService;
+    const colors: IColorSet = core._colorManager.colors;
     this._renderer = new WebglRenderer(terminal, colors, characterJoinerService, coreBrowserService, coreService, decorationService, this._preserveDrawingBuffer);
     forwardEvent(this._renderer.onContextLoss, this._onContextLoss);
     forwardEvent(this._renderer.onChangeTextureAtlas, this._onChangeTextureAtlas);
