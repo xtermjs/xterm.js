c51a0b745439b60cda5c49d4ecf2873bf08e1dae
arencoskun <ar3n1596@gmail.com>
2024-02-01 16:05:02 +0300

    Fix linter errors (hopefully)

--- typings/xterm.d.ts
+++ typings/xterm.d.ts
@@ -962,14 +962,16 @@ declare module '@xterm/xterm' {
      * Focus the terminal.
      */
     focus(): void;
-    
+
     /**
      * Input data to application side.
-     * The data is treated the same way as typed input at the terminal (will appear in the onData event).
+     * The data is treated the same way as typed input at the terminal
+     * (will appear in the onData event).
      * wasUserInput indicates, whether the input is genuine user input.
-     * It is true by default and triggers additional actions like prompt focus or selection clearing.
-     * Set it to false if your data sent does not resemble what a user would have typed
-     * (e.g. sequence embedded data).
+     * It is true by default and triggers additional actions like prompt 
+     * focus or selection clearing.
+     * Set it to false if your data sent does not resemble
+     * what a user would have typed (e.g. sequence embedded data).
      */
     input(data: string, wasUserInput?: boolean): void;
 
