ab2cb9a80922ab48a71cf35d9cceb694e7cd1fb7
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-07-03 02:33:17 -0700

    Migrate unicode-graphemes api tests to playwright

--- bin/test_playwright.js
+++ bin/test_playwright.js
@@ -18,14 +18,15 @@ while (argv.some(e => e.startsWith('--suite='))) {
 }
 
 let configs = [
-  { name: 'core',            path: 'out-test/playwright/playwright.config.js' },
-  { name: 'addon-attach',    path: 'addons/addon-attach/out-test/playwright.config.js' },
-  { name: 'addon-canvas',    path: 'addons/addon-canvas/out-test/playwright.config.js' },
-  { name: 'addon-clipboard', path: 'addons/addon-clipboard/out-test/playwright.config.js' },
-  { name: 'addon-fit',       path: 'addons/addon-fit/out-test/playwright.config.js' },
-  { name: 'addon-search',    path: 'addons/addon-search/out-test/playwright.config.js' },
-  { name: 'addon-serialize', path: 'addons/addon-serialize/out-test/playwright.config.js' },
-  { name: 'addon-webgl',     path: 'addons/addon-webgl/out-test/playwright.config.js' }
+  { name: 'core',                    path: 'out-test/playwright/playwright.config.js' },
+  { name: 'addon-attach',            path: 'addons/addon-attach/out-test/playwright.config.js' },
+  { name: 'addon-canvas',            path: 'addons/addon-canvas/out-test/playwright.config.js' },
+  { name: 'addon-clipboard',         path: 'addons/addon-clipboard/out-test/playwright.config.js' },
+  { name: 'addon-fit',               path: 'addons/addon-fit/out-test/playwright.config.js' },
+  { name: 'addon-search',            path: 'addons/addon-search/out-test/playwright.config.js' },
+  { name: 'addon-serialize',         path: 'addons/addon-serialize/out-test/playwright.config.js' },
+  { name: 'addon-unicode-graphemes', path: 'addons/addon-unicode-graphemes/out-test/playwright.config.js' },
+  { name: 'addon-webgl',             path: 'addons/addon-webgl/out-test/playwright.config.js' }
 ];
 
 if (suiteFilter) {
