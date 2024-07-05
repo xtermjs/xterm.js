be49bb6263df4611a940000596abd271ddda2014
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-07-05 09:12:30 -0700

    Move integration tests onto esbuild sources (except image)

--- bin/test_playwright.js
+++ bin/test_playwright.js
@@ -18,18 +18,19 @@ while (argv.some(e => e.startsWith('--suite='))) {
 }
 
 let configs = [
-  { name: 'core',                    path: 'out-test/playwright/playwright.config.js' },
-  { name: 'addon-attach',            path: 'addons/addon-attach/out-test/playwright.config.js' },
-  { name: 'addon-canvas',            path: 'addons/addon-canvas/out-test/playwright.config.js' },
-  { name: 'addon-clipboard',         path: 'addons/addon-clipboard/out-test/playwright.config.js' },
-  { name: 'addon-fit',               path: 'addons/addon-fit/out-test/playwright.config.js' },
+  { name: 'core',                    path: 'out-esbuild-test/playwright/playwright.config.js' },
+  { name: 'addon-attach',            path: 'addons/addon-attach/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-canvas',            path: 'addons/addon-canvas/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-clipboard',         path: 'addons/addon-clipboard/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-fit',               path: 'addons/addon-fit/out-esbuild-test/playwright.config.js' },
+  // TODO: Move onto esbuild sources
   { name: 'addon-image',             path: 'addons/addon-image/out-test/playwright.config.js' },
-  { name: 'addon-search',            path: 'addons/addon-search/out-test/playwright.config.js' },
-  { name: 'addon-serialize',         path: 'addons/addon-serialize/out-test/playwright.config.js' },
-  { name: 'addon-unicode-graphemes', path: 'addons/addon-unicode-graphemes/out-test/playwright.config.js' },
-  { name: 'addon-unicode11',         path: 'addons/addon-unicode11/out-test/playwright.config.js' },
-  { name: 'addon-web-links',         path: 'addons/addon-web-links/out-test/playwright.config.js' },
-  { name: 'addon-webgl',             path: 'addons/addon-webgl/out-test/playwright.config.js' }
+  { name: 'addon-search',            path: 'addons/addon-search/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-serialize',         path: 'addons/addon-serialize/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-unicode-graphemes', path: 'addons/addon-unicode-graphemes/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-unicode11',         path: 'addons/addon-unicode11/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-web-links',         path: 'addons/addon-web-links/out-esbuild-test/playwright.config.js' },
+  { name: 'addon-webgl',             path: 'addons/addon-webgl/out-esbuild-test/playwright.config.js' }
 ];
 
 if (suiteFilter) {
