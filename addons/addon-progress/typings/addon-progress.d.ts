75e5cc19e02684ea4311d1db76f9edc8806a4a71
jerch <jerch@rockborn.de>
2025-01-09 01:06:18 +0100

    Update addons/addon-progress/typings/addon-progress.d.ts
    Co-authored-by: Daniel Imms <2193314+Tyriar@users.noreply.github.com>

--- addons/addon-progress/typings/addon-progress.d.ts
+++ addons/addon-progress/typings/addon-progress.d.ts
@@ -8,19 +8,42 @@ import type { Event } from 'vs/base/common/event';
 import type { ProgressState } from '../src/ProgressAddon';
 
 declare module '@xterm/addon-progress' {
-  /** xterm.js addon providing an interface for ConEmu's progress sequence */
+  /**
+   * An xterm.js addon that provides an interface for ConEmu's progress
+   * sequence.
+   */
   export class ProgressAddon implements ITerminalAddon, IDisposable {
+    
+    /**
+     * Creates a new progress addon
+     */
     constructor();
+
+    /**
+     * Activates the addon
+     * @param terminal The terminal the addon is being loaded in.
+     */
     public activate(terminal: Terminal): void;
+    
+    /**
+     * Disposes the addon.
+     */
     public dispose(): void;
 
+    /**
+     * An event that fires when the tracked progress changes.
+     */
     public readonly onChange: Event<IProgress> | undefined;
 
-    /** getter / setter for current progress */
+    /**
+     * Gets or sets the current progress tracked by the addon.
+     */
     public progress: IProgress;
   }
   
-  /** progress object interface */
+  /**
+   * Progress tracked by the addon.
+   */
   export interface IProgress {
     state: ProgressState;
     value: number;
