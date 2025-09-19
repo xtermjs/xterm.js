ffd6dc6ab7f136440ccf3a7fcbd026bf1586c8fa
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-09-19 09:06:53 -0700

    Fix import issue

--- addons/addon-search/src/SearchEngine.test.ts
+++ addons/addon-search/src/SearchEngine.test.ts
@@ -5,7 +5,7 @@
 import { assert } from 'chai';
 import { SearchEngine } from './SearchEngine';
 import { SearchLineCache } from './SearchLineCache';
-import { Terminal } from '@xterm/xterm';
+import { Terminal } from 'browser/public/Terminal';
 import type { ISearchOptions } from '@xterm/addon-search';
 import { DisposableStore } from 'vs/base/common/lifecycle';
 
