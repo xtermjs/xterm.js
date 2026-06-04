da72f622468f15b1176bc250c29ebc47a55730cb
Jörg Breitbart <jerch@rockborn.de>
2026-06-04 12:46:57 +0200

    fix non-ascii clipboard handling

--- addons/addon-clipboard/src/ClipboardAddon.ts
+++ addons/addon-clipboard/src/ClipboardAddon.ts
@@ -78,13 +78,43 @@ export class BrowserClipboardProvider implements IClipboardProvider {
   }
 }
 
+/**
+ * TODO: Once the base64 handling on Uint8Arrays is more common,
+ * remove the btoa/atob fallbacks below.
+ */
+interface Uint8ArrayExt extends Uint8Array {
+  toBase64(): string;
+}
+interface Uint8ArrayExtCtor extends Uint8ArrayConstructor {
+  fromBase64(s: string): Uint8ArrayExt;
+}
+
 export class Base64 implements IBase64 {
   public encodeText(data: string): string {
-    return btoa(data);
+    const bytes = new TextEncoder().encode(data) as Uint8ArrayExt;
+    if (bytes.toBase64 !== undefined) {
+      return bytes.toBase64();
+    }
+    let bin = '';
+    for (let i = 0; i < bytes.length; i++) {
+      bin += String.fromCharCode(bytes[i]);
+    }
+    return btoa(bin);
   }
   public decodeText(data: string): string {
+    if ((Uint8Array as Uint8ArrayExtCtor).fromBase64 !== undefined) {
+      try {
+        return new TextDecoder().decode((Uint8Array as Uint8ArrayExtCtor).fromBase64(data));
+      } catch {}
+      return '';
+    }
     try {
-      return atob(data);
+      const bin = atob(data);
+      const bytes = new Uint8Array(bin.length);
+      for (let i = 0; i < bin.length; ++i) {
+        bytes[i] = bin.charCodeAt(i);
+      }
+      return new TextDecoder().decode(bytes);
     } catch {}
     return '';
   }
